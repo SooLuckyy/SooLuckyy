9b78fc0571815135b6b0219abd702338e30d029b
SooLuckyy <97842877+SooLuckyy@users.noreply.github.com>
2025-09-15 21:40:31 +0100

    Update wordel.py
    Fixed couple of things
    - Updated URL as it wasn't working anymore
    - Now actually randomly generate words
    - "target" word no longer prints from the start
    - Game now terminates when all 5 lives are used up
    - Added win/lose message

--- wordel.py
+++ wordel.py
@@ -3,10 +3,10 @@ from random import randint
 
 def main():
 
-    gen_5lw()
+    word = gen_5lw()
     welcome()
-    start_game(gen_5lw())
-    end_game()
+    wl = start_game(word)
+    end_game(wl, word)
 
 def start_game(target):
     print(target)
@@ -16,7 +16,7 @@ def start_game(target):
     l = ["_", "_", "_", "_", "_"]
     while True:
         if user_guess == target:
-            break
+            return "W"
         for index, letter in enumerate(user_guess):
             if letter in target:
                 if index == target.index(letter):
@@ -25,17 +25,26 @@ def start_game(target):
                 l[index] = f"{letter}*"
 
         attempts -= 1
+        # Checks if all lives are gone
+        if attempts == -1:
+            return "L"
+
         print("Attempts remaining:", attempts)
         print(" ".join(l))
         l = ["_", "_", "_", "_", "_"]
         user_guess = input("Guess: ")
         check_word(user_guess)
 
-def end_game():
-    print("You got it! Thanks for playing")
+
+def end_game(wl, word):
+    if wl == "W":
+        print(f"You got it! The word was {word}! Thanks for playing!")
+    else:
+        print(f"Unlucky! The word was {word}! Thanks for playing!")
+
 
 def check_word(s):
-    url = "https://cheaderthecoder.github.io/5-Letter-words/words.json"
+    url = "https://darkermango.github.io/5-Letter-words/words.json"
     response = requests.get(url)
     words = response.json()
     while True:
@@ -46,15 +55,15 @@ def check_word(s):
 
 def welcome():
     print("Welcome to wordel :) \nA letter with * means the letter appears in the word but different position")
-    print("For a full list of available words, please visit\nhttps://cheaderthecoder.github.io/5-Letter-words/words.txt")
+    print("For a full list of available words, please visit\nhttps://darkermango.github.io/5-Letter-words/words.txt")
     print("_ " * 5)
 
 def gen_5lw():
     # Generate a random 5 letter word
-    url = "https://cheaderthecoder.github.io/5-Letter-words/words.json"
+    url = "https://darkermango.github.io/5-Letter-words/words.json"
     response = requests.get(url)
     words = response.json()
-    rand_int = randint(0,len(words))
+    rand_int = randint(0,len(words["words"]))
     return words["words"][rand_int]
 
 
